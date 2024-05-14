8e50ec1ffe4134d14db0244f2f0fe76a626ca2b4
manugp <manuelgarces@iesportada.org>
2024-05-14 11:08:56 +0200

    Change readme.md

--- ServicioInyeccionDependenciasV2/Program.cs
+++ ServicioInyeccionDependenciasV2/Program.cs
@@ -11,7 +11,7 @@ namespace ServicioInyeccionDependencia
             var communicationService = new CommunicationService();
 
             var customers = customerService.GetCustomers();
-            var message = "¡Hola! Su pedido ya está disponible para recoger. Gracias por usar nuestro servicio de email";
+            var message = "¡Hola! Su pedido ya está disponible, se lo llevamos a casa. Gracias por usar nuestro servicio de email";
             foreach (var customer in customers)
             {
                 communicationService.SendMessage(customer.Email, message);
