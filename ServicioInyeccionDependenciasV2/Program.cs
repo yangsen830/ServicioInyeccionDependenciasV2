1ffde91dddf261f52a6f9137c7ede85a2fea249a
YangsenL <al.yangsen.liu@iesportada.org>
2024-05-14 18:35:41 +0200

    Add minor changes

--- ServicioInyeccionDependenciasV2/Program.cs
+++ ServicioInyeccionDependenciasV2/Program.cs
@@ -12,10 +12,15 @@ namespace ServicioInyeccionDependencia
 
             var customers = customerService.GetCustomers();
 <<<<<<< HEAD
+<<<<<<< HEAD
             var message = "¡Hola! Su pedido ya está disponible para recoger. Gracias por usar nuestro servicio de mensajería";
 =======
             var message = "¡Hola! Su pedido ya está disponible, se lo llevamos a casa. Gracias por usar nuestro servicio de email";
 >>>>>>> 8e50ec1 (Change readme.md)
+=======
+            var message = "¡Hola! Su pedido ya está disponible para recoger. Gracias por usar nuestro servicio de mensajería";
+
+>>>>>>> f2c84dc (Add minor changes)
             foreach (var customer in customers)
             {
                 communicationService.SendMessage(customer.Email, message);
